09c092cbc145138793b537f9bc603b5404fc0bc9
Laizy7 <ehrenvisteano7@gmail.com>
2023-10-16 21:09:49 +0700

    Berisi useToggle-Timeout-Debounce-UpdateEffect-Array

---
+++ custom-hooks-1-5/src/App.jsx
@@ -0,0 +1,15 @@
+// import ToggleComponent from './1-useToggle/ToggleComponent'
+// import TimeoutComponent from './2-useTimeout/TimeoutComponent'
+// import DebounceComponent from './3-useDebounce/DebounceComponent'
+// import UpdateEffectComponent from './4-useUpdateEffect/UpdateEffectComponent'
+import ArrayComponent from './5-useArray/ArrayComponent'
+
+function App() {
+  // return <ToggleComponent />
+  // return <TimeoutComponent />
+  // return <DebounceComponent />
+  // return <UpdateEffectComponent /> // Remove StrictMode to use this
+  return <ArrayComponent />
+}
+
+export default App
